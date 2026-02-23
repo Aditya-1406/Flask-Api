c989161f36c2b22cf5f9cb57f7ec7a6fe1dba113
Aditya-1406 <gargaditya674@gmail.com>
2026-02-23 12:16:29 +0530

    api using restful completed

--- Day3/api.py
+++ Day3/api.py
@@ -1,9 +1,11 @@
 from flask import Flask,request,jsonify
 from flask_sqlalchemy import SQLAlchemy
+from flask_restful import Resource, Api,reqparse,marshal_with,abort,fields
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 db = SQLAlchemy(app)
+api = Api(app)
 
 class User(db.Model):
     id = db.Column(db.Integer,primary_key= True)
@@ -12,6 +14,63 @@ class User(db.Model):
 
     def __repr__(self):
         return f"User(name = {self.name}, email = {self.email})"
+
+user_args = reqparse.RequestParser()
+user_args.add_argument('name',type=str,required = True,help="Name Should not be empty")
+user_args.add_argument('email',type=str,required = True,help="Email Should not be empty")
+
+userFields = {
+    'id': fields.Integer,
+    'name':fields.String,
+    'email':fields.String,
+}
+
+class UsersApi(Resource):
+    @marshal_with(userFields)
+    def get(self):
+        user = User.query.all()
+        return user
+    
+    @marshal_with(userFields)
+    def post(self):
+        args = user_args.parse_args()
+        user = User(name=args["name"],email = args["email"])
+        db.session.add(user)
+        db.session.commit()
+        users = User.query.all()
+        return users, 201
+    
+class UserApi(Resource):
+    @marshal_with(userFields)
+    def get(self,id):
+        user = User.query.filter_by(id=id).first()
+        if not user:
+            abort(404,message = "User not Found")
+        return user
+    
+    @marshal_with(userFields)
+    def patch(self,id):
+        args = user_args.parse_args()
+        user = User.query.filter_by(id=id).first()
+        if not user:
+            abort(404,message = "User not Found")
+        user.name = args["name"]
+        user.email = args["email"]
+        db.session.commit()
+        return user
+    
+    @marshal_with(userFields)
+    def delete(self,id):
+        user = User.query.filter_by(id=id).first()
+        if not user:
+            abort(404,message = "User not Found")
+        db.session.delete(user)
+        db.session.commit()
+        users = User.query.all()
+        return users, 204
+    
+api.add_resource(UsersApi,'/api/users')
+api.add_resource(UserApi,'/api/users/<int:id>')
 
 
 if __name__ == "__main__":
